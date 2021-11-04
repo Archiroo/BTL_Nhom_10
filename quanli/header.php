916b610a65c6be3c72cd5a2ecb67113f5dad19ce
Archiroo <newbieit.nb@gmail.com>
2021-11-04 17:15:21 +0700

    thêm giao diện

---
+++ quanli/header.php
@@ -0,0 +1,91 @@
+<?php
+    include('connect_database/connect.php');
+    // if(!isset($_SESSION['login-admin'])) //nếu khác admin thì ra ngoài
+    // {
+    //     header("Location:../index.php");
+    // }
+
+
+?>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
+    <link rel="stylesheet" href="css/quanli.css">
+    <title>Management website</title>
+</head>
+
+<body>
+    <input type="checkbox" id="sidebar-toggle">
+    <div class="sidebar">
+        <div class="sidebar-header">
+            <h3 class="brand">
+                <span class="las la-store"></span>
+                <span>Archiro</span>
+            </h3>
+            <label for="sidebar-toggle" class="ti-menu-alt"></label>
+            <!--nút 3 ghạch-->
+        </div>
+        <div class="sidebar-menu">
+            <ul>
+                <li>
+                    <a href="index.php">
+                        <span class="fas fa-home"></span>
+                        <span>Trang chủ</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="user.php">
+                        <span class="fas fa-user"></span>
+                        <span>Nhân viên</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="student.php">
+                        <span class="fas fa-user-friends"></span>
+                        <span>Khách hàng</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="subject.php">
+                        <span class="fas fa-folder"></span>
+                        <span>Sản phẩm</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="homework.php">
+                        <span class="fas fa-paste"></span>
+                        <span>Hóa đơn</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="contact.php">
+                        <span class="fas fa-poll"></span>
+                        <span>Thống kê</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="../sign-out.php">
+                        <span class="fas fa-power-off"></span>
+                        <span>Đăng xuất</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <!-- SEARCH -->
+    <div class="main-content">
+        <header>
+            <div class="search-wrapper">
+                <span class="ti-search"></span>
+                <input type="text" placeholder="Tìm kiếm" name="search">
+            </div>
+            <div class="social-icons">
+                <div>
+                    <img src="../image/nhanvien1.jpg" alt="">
+                </div>
+            </div>
+        </header> 
--- quanli/index.php
+++ quanli/index.php
@@ -1,49 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+    include('header.php');
+?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <?php
-    include('connect_database/connect.php');
-    ?>
-    <table>
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>User ID</th>
-            </tr>
-        </thead>
-        <tbody>
-            <!-- CODE PHP -->
-            <?php
-                $sql = "SELECT * FROM NHACUNGCAP";
-                $res = sqlsrv_query($conn, $sql);
-                   
-                        while ($row = sqlsrv_fetch_array($res)) {
-                            $id_std = $row['Ma_ncc'];
-                            $id_user = $row['Ten_ncc'];
-            ?>
-                        <tr>
-                            <td><?php  echo $id_std; ?></td>
-                            <td><?php  echo $id_user;?></td>
-                        </tr>
-            <?php
-                        
-                    
-                }
-                           
-             
-            ?>
-
-
-        </tbody>
-    </table>
-</body>
-
-</html>
+<?php 
+    include('footer.php');
+?>
